ce25f84f812dcb5e9df2ab4f4d31669f3afa6703
vitorgmorigi <vitorgarcezmorigi@gmail.com>
2020-11-17 22:30:31 -0300

    Adjusts for compile and run with MPI

--- replicacao.c
+++ replicacao.c
@@ -47,7 +47,7 @@ void insere(FILE* ptr) {
     fprintf(ptr, "[%s]\t[%d]\t[%.2f]\n", p.nome, p.idade, p.altura);
 }
 
-int main(&argc, &argv)
+int main(int argc, char** argv)
 {
     FILE* ptr;
     char* filename = "./arq_teste.txt";
@@ -55,6 +55,8 @@ int main(&argc, &argv)
 
     MPI_Init(&argc, &argv);
     MPI_Status status;
+
+    int rank, size;
 
     MPI_Comm_size(MPI_COMM_WORLD, &size);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
@@ -75,34 +77,35 @@ int main(&argc, &argv)
     MPI_Type_commit(&stat_type);
 
     // Struct para MPI criada com sucesso //
-    
-    if(opcao == 1)
 
-    if(rank == 0){
-	buffer = AbreArquivo();
+    FILE* buffer = fopen(filename, modo_gravacao);
+
+    if(rank == 0) {
 	
 	MPI_Send(&buffer, 1, stat_type, 1, MPI_ANY_TAG, MPI_COMM_WORLD);
 	printf("front-end enviando dados atraves de um buffer para o replica-manager-1");
-    }else{
-	if(rank == 1){
+    } else {
+	if (rank == 1) {
 		MPI_Recv(&buffer, 1, stat_type, 0, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
 		//funcao para adicionar o conteudo de buffer no arquivo de texto *//
-		abreArquivo();
-		escreveArquivo();
+		// abreArquivo();
+		// escreveArquivo();
 		printf("replica-manager-1 recebeu o buffer do front-end e adicionou o conteudo no banco-de-dados");
 
-		int iterator = rank + 1;
+		int iterator;
 
-		for(iterator < 3; iterator++){
+		for(iterator = rank + 1; iterator < 3; iterator++){
 			MPI_Send(&buffer, 1, stat_type, iterator, MPI_ANY_TAG, MPI_COMM_WORLD);
 			printf("replica-manager-1 replicando o banco-de-dados para o replica-manager%d \n",iterator); 	
 		}
-	}else if(rank > 1){
-		for(iterator < 3; iterator ++){
+	} else if(rank > 1) {
+
+        int iterator;
+		for(iterator = rank + 1; iterator < 3; iterator++) {
 			MPI_Recv(&buffer, 1, stat_type, 1, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
 			//funcao para adicionar o conteudo de buffer no arquivo de texto-backup *//
-			abreArquivo();
-			escreveArquivo();
+			// abreArquivo();
+			// escreveArquivo();
 			printf("replica-manager atualizado e consistente, backup realizado com sucesso");
 		}
 	}
