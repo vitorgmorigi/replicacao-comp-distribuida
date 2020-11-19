adef88ff4beaebd4604a3308f97b682ab8c9c1fd
root <root@web.franke.julio.vms.ufsc.br>
2020-11-19 20:18:51 -0200

    Versao 1.0

--- replicacao.c
+++ replicacao.c
@@ -23,17 +23,28 @@ int menuInicial() {
     return opcaoEscolhida;
 }
 
-void abreArquivo(){}
+FILE* abreArquivoW(FILE* arquivo, char* filename, char* permissao){ 
 
-void fechaArquivo(){}
+    arquivo = fopen(filename,permissao); 
+    
+    return arquivo;
+}
 
-void listaArquivo(){}
+void escreveArquivo(FILE* arquivo, pessoa_p pessoa, char* filename, char* permissao){
+   
+    abreArquivoW(arquivo, filename, permissao);
+    fwrite(&pessoa, sizeof(pessoa_p), 1, arquivo); 
+    fclose(arquivo);
+}
+
+void listaArquivo(){
+}
 
 void insere(FILE* ptr) {
     pessoa_p p;
 
     printf("Digite o nome: ");
-    scanf("%s", &p.nome);
+    scanf("%19s", p.nome);
     printf("\n");
 
     printf("Digite a idade: ");
@@ -47,91 +58,117 @@ void insere(FILE* ptr) {
     fprintf(ptr, "[%s]\t[%d]\t[%.2f]\n", p.nome, p.idade, p.altura);
 }
 
-int main(int argc, char** argv)
-{
-    FILE* ptr;
-    char* filename = "./arq_teste.txt";
-    char* modo_gravacao = "a"; // modo de gravação "append" (adiciona no final)
+int main(int argc, char** argv){
 
-    MPI_Init(&argc, &argv);
-    MPI_Status status;
+    int opcaoEscolhida;
 
-    int rank, size;
+    opcaoEscolhida = menuInicial();
 
-    MPI_Comm_size(MPI_COMM_WORLD, &size);
-    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    if(opcaoEscolhida == 1){	
+	
+        MPI_Init(&argc, &argv);
+    	MPI_Status status;
 
-    // Convertendo uma struct em C para o MPI, o MPI por padrao nao reconhece struct, ele nao eh nativo a linguagem C *//    
+    	int rank, size;    
 
-    const int qtd = 3;
-    int qtdBlocos [3] = {1,1,1};
-    MPI_Datatype tipos[3] = {MPI_CHAR,MPI_UNSIGNED,MPI_FLOAT};
-    MPI_Datatype stat_type;
-    MPI_Aint offsets[3];			//Vetor que segmenta os offsets para cada variavel pertencente a struct *//
+    	MPI_Comm_size(MPI_COMM_WORLD, &size);
+    	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-    offsets[1] = offsetof(pessoa_p, nome);	//Shift bytes, passando a referencia da struct e o tamanho da variavel para cada bloco *//
-    offsets[2] = offsetof(pessoa_p, idade);
-    offsets[3] = offsetof(pessoa_p, altura);
+    	// Convertendo uma struct em C para o MPI, o MPI por padrao nao reconhece struct, ele nao eh nativo a linguagem C *//    
+
+    	const int qtd = 3;
+    	int tamanhoBlocos[qtd];
+    	MPI_Datatype tipos[qtd];
+    	MPI_Datatype stat_type;
+    	MPI_Aint char_length, int_length, offsets[qtd];			//Vetor que segmenta os offsets para cada variavel pertencente a struct *//   
+  
+		
+    	tamanhoBlocos[0] = 30;
+	tamanhoBlocos[1] = 1;
+	tamanhoBlocos[2] = 1;
+
+    	tipos[0] = MPI_CHAR;
+	tipos[1] = MPI_UNSIGNED;
+	tipos[2] = MPI_DOUBLE;
+
+	offsets[0] = 0; 
+    	offsets[1] = char_length * 30;
+	offsets[2] = char_length * 30 + int_length;
+//	offsets[2] = (size_t)&(pessoa_p.altura) - (size_t)&pessoa_p;
     
-    MPI_Type_create_struct(qtd, qtdBlocos, offsets, tipos, &stat_type);
-    MPI_Type_commit(&stat_type);
+    	MPI_Type_create_struct(qtd, tamanhoBlocos, offsets, tipos, &stat_type);
+    	MPI_Type_commit(&stat_type);
 
     // Struct para MPI criada com sucesso //
-
-    FILE* buffer = fopen(filename, modo_gravacao);
-
-    if(rank == 0) {
+	if(rank == 0) {
 	
-	MPI_Send(&buffer, 1, stat_type, 1, MPI_ANY_TAG, MPI_COMM_WORLD);
-	printf("front-end enviando dados atraves de um buffer para o replica-manager-1");
+	pessoa_p pessoa;
+         
+	scanf("%s", pessoa.nome);
+        scanf("%d", &pessoa.idade);
+        scanf("%f", &pessoa.altura);
+        
+	MPI_Send(&pessoa, 1, stat_type, 1, MPI_ANY_TAG, MPI_COMM_WORLD);
+	printf("front-end enviando dados atraves de um buffer para o replica-manager-1\n");
     } else {
 	if (rank == 1) {
-		MPI_Recv(&buffer, 1, stat_type, 0, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
-		//funcao para adicionar o conteudo de buffer no arquivo de texto *//
-		// abreArquivo();
-		// escreveArquivo();
-		printf("replica-manager-1 recebeu o buffer do front-end e adicionou o conteudo no banco-de-dados");
+		pessoa_p pessoa;
+		FILE* ptr;
+		char* filename = ("replica-manager.txt");
+		char* permissao = "a";
+		
+		MPI_Recv(&pessoa, 1, stat_type, 0, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+		escreveArquivo(ptr, pessoa, filename, permissao);
+		printf("replica-manager-1 recebeu o buffer do front-end e adicionou o conteudo no banco-de-dados\n");
 
 		int iterator;
 
 		for(iterator = rank + 1; iterator < 3; iterator++){
-			MPI_Send(&buffer, 1, stat_type, iterator, MPI_ANY_TAG, MPI_COMM_WORLD);
-			printf("replica-manager-1 replicando o banco-de-dados para o replica-manager%d \n",iterator); 	
+			MPI_Send(&pessoa, 1, stat_type, iterator, MPI_ANY_TAG, MPI_COMM_WORLD);
+			printf("replica-manager-%d replicou banco-de-dados para o replica-manager%d \n",rank,iterator); 	
 		}
-	} else if(rank > 1) {
+	} else if (rank > 1) {
+		pessoa_p pessoa;
+		FILE* ptr;
+		char* filename;		
 
-        int iterator;
-		for(iterator = rank + 1; iterator < 3; iterator++) {
-			MPI_Recv(&buffer, 1, stat_type, 1, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
-			//funcao para adicionar o conteudo de buffer no arquivo de texto-backup *//
-			// abreArquivo();
-			// escreveArquivo();
-			printf("replica-manager atualizado e consistente, backup realizado com sucesso");
+		if(rank == 2){        	
+			filename = ("replica-manager-2.txt");
+		}else if(rank == 3){
+			filename = ("replica-manager-3.txt");
 		}
-	}
-    }
-    
+		char* permissao = "a";		
+	
+		MPI_Recv(&pessoa, 1, stat_type, 1, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+		escreveArquivo(ptr, pessoa, filename, permissao);
+		printf("replica-manager-%d atualizado e consistente, backup realizado com sucesso\n",rank);
+		}
+  	}
 
-    if ((ptr = fopen(filename, modo_gravacao)) == NULL) {
-        puts("Erro ao abrir o arquivo!");
-        return 1;
-    }
+	MPI_Barrier(MPI_COMM_WORLD);
+	MPI_Type_free(&stat_type);
+	MPI_Finalize();
+    }    
 
-    int opcaoEscolhida = menuInicial();
+//    if ((ptr = fopen(filename, modo_gravacao)) == NULL) {
+//        puts("Erro ao abrir o arquivo!");
+//        return 1;
+//    }
 
-    if(opcaoEscolhida == 1) {
-        insere(ptr);
-    } else if(opcaoEscolhida == 2) {
-        printf("Ainda nao implementado...");
-    } else {
-        printf("Ainda nao implementado...");
-    }
+//    int opcaoEscolhida = menuInicial();
+
+//    if(opcaoEscolhida == 1) {
+//        insere(ptr);
+//    } else if(opcaoEscolhida == 2) {
+//        printf("Ainda nao implementado...");
+//    } else {
+//        printf("Ainda nao implementado...");
+//    }
 
 
-    fclose(ptr);
+//    fclose(ptr);
 
-    puts("Arquivo gravado com sucesso!");
+//    puts("Arquivo gravado com sucesso!");
 
-    MPI_Finalize();
     return 0;
 }
